41e12fa5f5d669c13cf12cf774095e2fb8751853
ducpb <ducpb07101999@gmail.com>
2021-04-16 10:32:17 +0700

    ducpb

---
+++ blocks/timeline/lang/vi/block_timeline.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the timeline block.
+ *
+ * @package    block_timeline
+ * @copyright  2018 Ryan Wyllie <ryan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['ariadayfilter'] = 'Lọc các items trên timeline';
+$string['ariadayfilteroption'] = 'Filter theo {$a}';
+$string['ariaeventlistitem'] = 'Hoạt động {$a->name} trong khóa học {$a->course} hết hạn vào {$a->date}';
+$string['ariaeventlistpagelimit'] = 'Show hoạt động {$a} cho mỗi page';
+$string['ariaeventlistpaginationnavdates'] = 'Phân trang các hoạt động timeline';
+$string['ariaeventlistpaginationnavcourses'] = 'Phân trang các hoạt động cho khóa học {$a}';
+$string['ariaviewselector'] = 'Sắp xếp các items trong timeline';
+$string['ariaviewselectoroption'] = 'Sắp xếp theo {$a}';
+$string['duedate'] = 'Ngày đáo hạn';
+$string['morecourses'] = 'Thêm khóa học';
+$string['timeline:myaddinstance'] = 'Thêm một khối timeline mới tới Dashboard';
+$string['nocoursesinprogress'] = 'Không có khóa học nào đang theo học';
+$string['noevents'] = 'Không có hoạt động nào sắp diễn ra';
+$string['next30days'] = 'Trong 30 ngày tới';
+$string['next7days'] = 'Trong 7 ngày tới';
+$string['next3months'] = 'Trong 3 tháng tới';
+$string['next6months'] = 'Trong 6 tháng tới';
+$string['overdue'] = 'Qúa hạn';
+$string['all'] = 'Tất cả';
+$string['pluginname'] = 'Timeline';
+$string['sortbycourses'] = 'Sắp xếp theo khóa học';
+$string['sortbydates'] = 'Sắp xếp theo thời gian';
+$string['timeline'] = 'Timeline';
+$string['viewcourse'] = 'View khóa học';
+$string['privacy:metadata:timelinesortpreference'] = 'Tùy chọn sắp xếp của người dùng cho timeline';
+$string['privacy:metadata:timelinefilterpreference'] = 'Tùy chọn bộ lọc ngày của người dùng cho timeline';
+$string['privacy:metadata:timelinelimitpreference'] = 'Tùy chọn giới hạn trang người dùng cho timeline';
