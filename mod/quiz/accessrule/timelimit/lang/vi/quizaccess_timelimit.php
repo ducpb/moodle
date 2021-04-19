8b80ee805099aa1c71645da196e9e852a297596b
ducpb <ducpb07101999@gmail.com>
2021-04-19 14:49:17 +0700

    ducpb

---
+++ mod/quiz/accessrule/timelimit/lang/vi/quizaccess_timelimit.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the quizaccess_timelimit plugin.
+ *
+ * @package    quizaccess
+ * @subpackage timelimit
+ * @copyright  2011 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$string ['confirmstartheader'] = 'Giới hạn thời gian';
+$string ['confirmstart'] = 'Bài thi của bạn sẽ có giới hạn thời gian là {$a}. Khi bạn bắt đầu, đồng hồ sẽ bắt đầu đếm ngược và không thể tạm dừng. Bạn phải hoàn thành bài thi của mình trước khi nó hết hạn. Bạn có chắc chắn muốn bắt đầu ngay bây giờ không? ';
+$string ['pluginname'] = 'Quy tắc truy cập bài kiểm tra giới hạn thời gian';
+$string ['privacy:metadata'] = 'Trình cắm quy tắc truy cập bài kiểm tra Giới hạn thời gian không lưu trữ bất kỳ dữ liệu cá nhân nào.';
+$string ['quiztimelimit'] = 'Giới hạn thời gian: {$a}';
